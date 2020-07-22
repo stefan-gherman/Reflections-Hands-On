88c3bf8a9659394fc1eaf6b4f3e60fb37d2db5e2
Stefan Gherman <scgheman@gmail.com>
2020-07-23 02:57:45 +0300

    Add solution for exercise 1

--- Reflections Hands-On/Program.cs
+++ Reflections Hands-On/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Reflections_Hands_On
 {
@@ -6,7 +7,24 @@ namespace Reflections_Hands_On
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            ShowAssembly(Assembly.LoadFile(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.dll"));
+            ShowAssembly(Assembly.GetExecutingAssembly());
+        }   
+
+        static void ShowAssembly (Assembly assembly)
+        {
+            Console.WriteLine($"Assembly full name:{assembly.FullName}, " +
+                $"Global Assembly Cache {assembly.GlobalAssemblyCache}, " +
+                $"Location: {assembly.Location}, " +
+                $"Image Runtime Version: {assembly.ImageRuntimeVersion}");
+
+            foreach(var module in assembly.Modules)
+            {
+                Console.WriteLine($"Current module name: {module.Name}");
+            }
+
+
         }
     }
 }
