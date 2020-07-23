40ee397afb267efe8fb53f4b495654b218174a79
Stefan Gherman <scgheman@gmail.com>
2020-07-23 19:07:53 +0300

    Add solution for exercise 5

---
+++ DemoDynamic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName assemblyName = new AssemblyName("This is a name");
+            assemblyName.Version = new Version("1.0.0");
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            AssemblyBuilder builder = currentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.ReflectionOnly);
+            ModuleBuilder module = builder.DefineDynamicModule("CodeModule", "DemoAssembly.dll");
+            TypeBuilder typeBuilder = module.DefineType("Horse");
+
+            Type currentType = typeBuilder.CreateType();
+            Console.WriteLine(currentType.FullName);
+
+            foreach(var info in currentType.GetMembers())
+            {
+                Console.WriteLine($"Member type: {info.MemberType}, name: {info.Name}");
+            }
+        }
+    }
+}
